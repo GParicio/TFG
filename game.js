ca9bf791bce3d027f08bb9d2ae4d6b3c486d9f19
GParicio <guillermo.paricio01@gmail.com>
2023-05-31 17:51:37 +0200

    Tags agregados

--- game.js
+++ game.js
@@ -2,14 +2,9 @@ const gameId = new URLSearchParams(window.location.search).get('id');
 
 const apiKey = '79495262damsh0ccac0e59f88553p108ecbjsn61567da6ba49'; // Reemplaza "YOUR_API_KEY" con tu propia clave API
 
-const gameTitle = document.getElementById('game-title');
-const gameReleased = document.getElementById('game-released');
-const gamePrice = document.getElementById('game-price');
-const gameDescription = document.getElementById('game-description');
-const gameImage = document.getElementById('game-image');
-const gameLink = document.getElementById('game-link');
+const gameTotal = document.getElementById('games-total');
+const gameBlank = document.getElementById('games-blank');
 
-const gameTotal = document.getElementById('games-list');
 
 
 const url = `https://steam2.p.rapidapi.com/appDetail/${gameId}`;
@@ -28,27 +23,24 @@ axios.request(options)
   .then(response => {
     
     const game = response.data;
+    const tagNames = game.tags.map(tag => tag.name);
 
     gameTotal.innerHTML = `
-          <a>test</a>
-          <h3><a href="game.html?id=${game.appId}">${game.title}</a></h3>
-          <p class="game-info"><span class="label">Released:</span> ${game.released}</p>
-          <p class="game-info"><span class="label">Price:</span> ${game.price}</p>
-          <img src="${game.imgUrl}" alt="${game.title}" class="game-image">
-          <a href="https://store.steampowered.com/app/${gameId}/" target="_blank" class="game-link">Ver en Steam</a>
-        `;
+      <h3>${game.title}</h3>
+      <p class="game-info"><span class="label">Released:</span> ${game.released}</p>
+      <p class="game-info"><span class="label">Price:</span> ${game.price}</p>
+      <p id="game-description" class="game-description">${game.description}</p>
+      <img src="${game.imgUrl}" alt="${game.title}" class="game-image">
+      <a href="https://store.steampowered.com/app/${gameId}/" target="_blank" class="game-link">Ver en Steam</a>
+      <p id="game-description" class="game-description">${tagNames.join(', ')}</p>
+    `;
+    
+    gameBlank.textContent = `Released: ${game.released}`;
 
-
+        
+    //gameTitle.textContent = game.title;
     
 
-    gameTitle.textContent = game.title;
-    gameReleased.textContent = `Released: ${game.released}`;
-    gamePrice.textContent = `Price: ${game.price}`;
-    gameDescription.textContent = game.description;
-    gameLink.textContent = game.appId;
-    gameImage.src = game.imgUrl;
-    gameLink.href = `https://store.steampowered.com/app/${gameId}/`;
-    gameLink.hrefName = `Ver en Steam`;
   })
   .catch(error => {
     console.error(error);
